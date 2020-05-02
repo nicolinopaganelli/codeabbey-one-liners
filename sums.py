65a2481f2c84bb4432948511707bf8f77b839cc8
nicolinopaganelli <54078029+nicolinopaganelli@users.noreply.github.com>
2020-05-01 17:15:02 -0700

    Update sums.py
    Returns the some of as many numbers separated by a space as you'd like

--- sums.py
+++ sums.py
@@ -1,5 +1,5 @@
-## Will return the some of as many numbers separated by a space as you'd like
-## ex)
+## Returns the some of as many numbers separated by a space as you'd like
+# ex)
 # IN: 1 2 3 4 5
 # OUT: 15
 print(sum(list(map(int, input('\n>> ').split(' ')))))
