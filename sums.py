48889e51aeb701a75fbae1329935a80c1f521dd8
nicolinopaganelli <54078029+nicolinopaganelli@users.noreply.github.com>
2020-06-09 10:16:39 -0700

    Fixed typo

--- sums.py
+++ sums.py
@@ -1,4 +1,4 @@
-## Returns the some of as many numbers separated by a space as you'd like
+## Returns the sum of as many numbers separated by a space as you'd like
 # ex)
 # IN: 1 2 3 4 5
 # OUT: 15
